eca572ceaf8c6271e7dfe63a8cc8cc693e87a99c
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-05-29 17:41:31 +0530

    gaurded routes added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { Switch, Route} from "react-router-dom";
+import GuardedRoute from "./gaurdedRoute";
 import LandingPage from "../pages/landingPage/landingPage";
 import HomePage from "../pages/homePage/homepage";
 import Signup from "../pages/signup/signup";
@@ -7,13 +8,16 @@ import Login from "../pages/login/login";
 import ResetPassword from "../pages/resetPassword/resetPassword";
 
 const Routes = () => {
+    
+    const[isAutheticated, setisAutheticated] = useState(false);
+
     return(
         <Switch>
             <Route path="/" component={LandingPage} exact />
-            <Route path="/home" component={HomePage} />
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
             <Route path="/resetpassword" component={ResetPassword} />
+            <GuardedRoute path="/home" component={HomePage} auth={isAutheticated} />
         </Switch>
     )
 }
