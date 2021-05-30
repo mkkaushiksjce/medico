dbfc98ca45718e072fca147e203bf461e1f25f03
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-05-30 21:44:03 +0530

    login layout

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./login.css";
 
-function Login (props) {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
+  const validateForm = () => {
+    return email.length > 0 && password.length > 0;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <div>
         <div>
-            Mayaskara Login Page
+          <span>Email</span>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-    )
-
+        <div>
+          <span>Password</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <input block size="lg" type="button" value="submit" disabled={!validateForm()}/>
+      </div>
+    </div>
+  );
 }
-
 export default Login;
